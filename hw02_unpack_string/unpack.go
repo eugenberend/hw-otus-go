ab978996ed57785059eb00909697c3e3499e56e9
eugenberend <evgeniy.berendyaev@gmail.com>
2022-07-05 18:03:43 +0300

    fix: non-letter behaviour

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -25,8 +25,8 @@ func Unpack(input string) (string, error) {
 			for i := 1; i <= int(char-'0'); i++ {
 				sb.WriteRune(prev)
 			}
-		} else if unicode.IsLetter(char) {
-			if unicode.IsLetter(prev) {
+		} else {
+			if pos != 0 && !unicode.IsDigit(prev) {
 				sb.WriteRune(prev)
 			}
 			if pos == len(str)-1 {
