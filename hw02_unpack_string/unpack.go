76b1042d1574aa7265e763a6287c342d0837d34e
Evgeniy Berendyayev <evgeniy.berendyaev@gmail.com>
2022-07-10 19:43:08 +0300

    [HW-02] Unpack string (#2)
    * [HW-02] Unpack string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+
+	str := []rune(input)
+
+	var sb strings.Builder
+	var prev rune
+	for pos, char := range str {
+		if IsDigit(char) {
+			if IsFirstRune(str, pos) || IsDigit(prev) {
+				return "", ErrInvalidString
+			}
+			RepeatRune(&sb, prev, int(char-'0'))
+		} else {
+			if !IsFirstRune(str, pos) && !IsDigit(prev) {
+				sb.WriteRune(prev)
+			}
+			if IsLastRune(str, pos) {
+				sb.WriteRune(char)
+			}
+		}
+		prev = char
+	}
+	return sb.String(), nil
+}
+
+func RepeatRune(sb *strings.Builder, r rune, num int) {
+	for i := 1; i <= num; i++ {
+		sb.WriteRune(r)
+	}
+}
+
+func IsFirstRune(str []rune, pos int) bool {
+	return pos == 0 && len(str) != 0
+}
+
+func IsLastRune(str []rune, pos int) bool {
+	return pos == len(str)-1 && len(str) != 0
+}
+
+func IsDigit(r rune) bool {
+	return unicode.IsDigit(r)
 }
