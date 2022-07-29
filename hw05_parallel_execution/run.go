846d67a7bc1991d9ca940560473bb1d24395f338
eugenberend <evgeniy.berendyaev@gmail.com>
2022-07-29 10:04:49 +0300

    [HW-05] feat: Worker farm for parralel execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,61 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var WorkersNumberNegative = errors.New("number of workers should be positive")
+
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	var ignoreErr bool
+	var result error
+
+	if m < 0 {
+		ignoreErr = true
+	}
+
+	if n <= 0 {
+		return WorkersNumberNegative
+	}
+
+	if len(tasks) < n {
+		n = len(tasks)
+	}
+
+	wg := new(sync.WaitGroup)
+	taskCh := make(chan Task)
+	var errCounter int32
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for task := range taskCh {
+				err := task()
+				if !ignoreErr && err != nil {
+					atomic.AddInt32(&errCounter, 1)
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		taskCh <- task
+		curErrCount := int(atomic.LoadInt32(&errCounter))
+		if !ignoreErr && curErrCount >= m {
+			result = ErrErrorsLimitExceeded
+			break
+		}
+	}
+	close(taskCh)
+	wg.Wait()
+
+	return result
 }
